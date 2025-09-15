cc55fb58e94ec5b7824232150baab0a2bd12503e
Martin Fuks <martinfukss@gmail.com>
2025-09-15 17:43:44 -0300

    vuelta atras back

--- src/app.js
+++ src/app.js
@@ -10,41 +10,32 @@ const server = express();
 
 server.name = "BizSupply API";
 
-// Orígenes permitidos
-const allowedOrigins = [
-  "https://bizupply.netlify.app",
-  "http://localhost:5173",
-  "http://localhost:8080",
-];
-
-// CORS correcto
 server.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      return callback(null, true);
-    }
-    return callback(new Error("Not allowed by CORS"));
-  },
-  credentials: true,
+  origin: 'https://bizupply.netlify.app',
 }));
-
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 
-// ❌ Eliminar este middleware que pisaba los headers (causa del error CORS)
-// server.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "http://localhost:8080", "http://localhost:5173");
-//   res.header("Access-Control-Allow-Credentials", "true");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-//   next();
-// });
+server.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "http://localhost:8080",
+    "http://localhost:5173" // Tu frontend de Vite
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 
 server.use("/api", routes);
 
-// Error handler
+// Error catching endware.
 server.use((err, req, res, next) => {
   const status = err.status || 500;
   const message = err.message || err;
