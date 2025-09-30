fc0cb12876bbba18bdb8bc0f3108ba1a88e5d8c7
Martin Fuks <martinfukss@gmail.com>
2025-09-30 13:53:33 -0300

    hotfix

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -238,7 +238,6 @@ const createUser = async (req, res, next) => {
         profile_image: profile_image || null,
         auth0_id: auth0_id,
         provider_id: null,
-        saved_provider_ids: [], // ✅ JSON string
       });
 
     console.log("User creado correctamente:", userCreated);
