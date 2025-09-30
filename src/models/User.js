9b9f23763293f033beddc9cce0e3c1abf2b0d7d7
Martin Fuks <martinfukss@gmail.com>
2025-09-30 13:52:48 -0300

    fix

--- src/models/User.js
+++ src/models/User.js
@@ -58,7 +58,7 @@ module.exports = (sequelize) => {
       },
       saved_provider_ids: {
         type: DataTypes.ARRAY(DataTypes.INTEGER),
-        allowNull: false,
+        allowNull: true,
         defaultValue: [],
       },
       saved: {
